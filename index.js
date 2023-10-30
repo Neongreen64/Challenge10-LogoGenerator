9ffc37b59674a403d3367ecf87647cc6725ff40f
neon <dogmanyo@gmail.com>
2023-10-30 14:58:39 -0400

    added usrInput.js and implemented logic. started working on the index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+//import fs
+const fs = require('fs');
+
+//import shape classes from shapes.js
+const {Circle,Square,Triangle} = require('./lib/shapes.js');
+
+//import the function getUsrInput from usrInput.js
+const {getUsrInput} = require('./lib/usrInput');
+
+//generateShape() async function
+async function generateShape(){
+    try{
+        const usrInput = await getUsrInput();
+        let shapeType;
+        if(usrInput.shape){
+
+        }
+    }
+    
+}
